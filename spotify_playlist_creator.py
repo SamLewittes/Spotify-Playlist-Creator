6af3acf747ac72fb36f4dcc8fccd82088bf469be
Sam Lewittes <srljmail3@gmail.com>
2019-03-05 21:31:49 -0500

    Layout of project

--- spotify_playlist_creator.py
+++ spotify_playlist_creator.py
@@ -6,3 +6,46 @@ from bs4 import BeautifulSoup
 import requests
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+
+class Playlist:
+	def __init__(self,artists,songs):
+		self.artists = artists
+		self.songs = songs
+
+
+def main():
+
+	playlist = Playlist(get_artists, get_songs)
+	create_playlist()
+	add_songs()
+	delete_old_playlist()
+
+	new_songs = get_new_songs()
+	play_count = get_play_count(new_songs())
+	create_graph(play_count)
+
+
+def get_artists():
+	pass
+
+def get_songs():
+	pass
+
+def create_playlist():
+	pass
+
+def add_songs():
+	pass
+
+def delete_old_playlist():
+	pass
+
+
+def get_new_songs():
+	pass
+
+def get_play_count():
+	pass
+
+def create_graph():
+	pass
