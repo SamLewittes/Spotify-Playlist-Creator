48ad724aa74f2cf0bd3b64ed45b13929e0337857
Sam Lewittes <srljmail3@gmail.com>
2019-03-05 21:08:41 -0500

    spotify_playlist_creator.py

---
+++ spotify_playlist_creator.py
@@ -0,0 +1,8 @@
+import csv
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from bs4 import BeautifulSoup
+import requests
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
