653f235593fad58c80a8c41147e5c7bdeda8af3f
Sam Lewittes <srljmail3@gmail.com>
2019-03-07 12:15:02 -0500

    get_songs and add_songs functions

--- spotify_playlist_creator.py
+++ spotify_playlist_creator.py
@@ -2,38 +2,20 @@ import csv
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from bs4 import BeautifulSoup
-import requests
+from selenium.webdriver import ActionChains
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import datetime
-
-class Playlist:
-	def __init__(self,artists,songs):
-		self.artists = artists
-		self.songs = songs
-
 
 def main():
 
 	driver = webdriver.Chrome(r'C:\Users\Sam Lewittes\Downloads\chromedriver\chromedriver.exe')
 
-	#playlist = Playlist(get_artists(driver), get_songs(driver))
-	#print(playlist.artists)
+	artists = get_artists(driver)
+	songs = get_songs(driver,artists)
+	create_playlist(driver)
+	add_songs(driver,songs)
 
-	spotify_login(driver)
-	#create_playlist(driver)
-	delete_old_playlist(driver)
-
-
-
-	#create_playlist()
-	#add_songs()
-	#delete_old_playlist()
-
-	#new_songs = get_new_songs()
-	#play_count = get_play_count(new_songs())
-	#create_graph(play_count)
 
 
 def get_artists(driver):
@@ -67,8 +49,22 @@ def spotify_login(driver):
 	driver.find_element_by_id('login-button').click()
 	sleep(.5)
 
-def get_songs(driver):
-	return[]
+def get_songs(driver, artists):
+
+	songs = []
+	
+	for artist in artists:
+		artist.replace(" ", "+")
+		driver.get('https://www.google.com/search?safe=strict&hl=en&source=hp&ei=B6uAXIiyFsqEtQXr7YiQBQ&q=new+' + artist + '+songs&btnK=Google+Search&oq=new+' + artist + '+songs&gs_l=psy-ab.3..0l2j0i22i30l8.5153.8057..8240...0.0..1.229.2246.15j5j2......0....1..gws-wiz.....0..0i131j0i3.eR4WOjQ7FQU')
+		sleep(.5)
+		list_of_dates = driver.find_elements_by_xpath("//*[@class='jbzYp']")
+		list_of_titles = driver.find_elements_by_xpath("//*[@class='title']")
+		for index in range(len(list_of_dates)):
+			if "2019" in list_of_dates[index].text:
+				songs.append(list_of_titles[index].text)
+
+	return list(set(songs))
+
 
 def create_playlist(driver):
 	
@@ -83,8 +79,34 @@ def create_playlist(driver):
 
 	sleep(.5)
 
-def add_songs():
-	pass
+def add_songs(driver, songs):
+	
+	for song in songs:
+		driver.get("https://open.spotify.com/search/recent")
+		sleep(.5)
+		driver.find_element_by_class_name("SearchInputBox__input").send_keys(song)
+		sleep(2)
+
+		action = ActionChains(driver)
+		action.move_to_element((driver.find_elements_by_xpath("//*[@class='tracklist-name ellipsis-one-line']"))[0]).perform();
+		action.context_click().perform()
+		sleep(.3)
+		driver.find_elements_by_xpath("//*[@class='react-contextmenu-item']")[3].click()
+		sleep(.3)
+
+
+		possibilities = driver.find_elements_by_xpath("//*[@class='mo-info-name']")
+		for index in range(len(possibilities)):
+			try:
+				if "New Songs - " in possibilities[index].get_attribute("title"):
+					possibilities[index].click()
+					sleep(.3)
+					driver.find_elements_by_xpath("//*[@class='mo-coverArt-hover']")[0].click()
+					sleep(.3)
+			except:
+				pass
+
+		
 
 def delete_old_playlist(driver):
 
@@ -99,13 +121,12 @@ def delete_old_playlist(driver):
 			driver.get(list_of_playlists[i].get_attribute("href"))
 			sleep(.5)
 
-			driver.find_element_by_xpath("//*[@class='btn btn-transparent btn--narrow']").click()
+			driver.find_elements_by_xpath("//*[@class='btn btn-transparent btn--narrow']")[0].click()
 			sleep(.5)
 			driver.find_elements_by_xpath("//*[@class='react-contextmenu-item']")[7].click()
 			sleep(.1)
 			driver.find_elements_by_xpath("//*[@class='button-group__item']")[1].click()
 			sleep(.5)
-
 
 
 def get_new_songs():
@@ -115,6 +136,9 @@ def get_play_count():
 	pass
 
 def create_graph():
+	pass
+
+def add_to_csv():
 	pass
 
 main()
